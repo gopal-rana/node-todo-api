092a06b8afbcbc22265e02a5c561b82f95757815
gopal <gopal.singh@niit-tech.com>
2018-08-02 05:55:52 -0400

    Add PATCH/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser  = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser  = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -67,6 +69,32 @@ app.delete('/todos/:id', (req, res) => {
 	});
 	
 });
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+	
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	
+	if(_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = true;
+		body.completedAt = null;
+	}
+	
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if(!todo){
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	})
+	
+})
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000');
