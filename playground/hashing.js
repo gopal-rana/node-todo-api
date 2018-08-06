0ea73e7061a7fd3bfc35aad728ceabeed31afeb6
gopal <gopal.singh@niit-tech.com>
2018-08-06 07:26:16 -0400

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,21 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
+var password = 'abc@123';
+
+bcrypt.genSalt(10, (err, salt) =>{
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	})
+});
+
+var hashedPassword = '$2a$10$76NcvK4l4BgD2tGPVl1W5OPVQCG.UrAkK4eKDJYvpn0Z/P1aSzj/i';
+bcrypt.compare(password, hashedPassword, (err, res) =>{
+	console.log(res);
+});
+
+/* var data = {
 	id: 10
 }
 
@@ -12,7 +26,7 @@ console.log(token);
 
 var decoded = jwt.verify(token, '123abc');
 
-console.log('decoded', decoded);
+console.log('decoded', decoded); */
 
 /* var message = 'I am user number 3';
 
