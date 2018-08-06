82a05f12525f25c1b8af7142492903edfd13a262
gopal <gopal.singh@niit-tech.com>
2018-08-06 07:00:09 -0400

    Add GET/users/me and authenticate

--- server/models/user.js
+++ server/models/user.js
@@ -38,7 +38,7 @@ UserSchema.methods.toJSON = function (){
 	return _.pick(userObj, ['_id', 'email']);
 }
 
-UserSchema.methods.generateAuthToken = function() {
+UserSchema.methods.generateAuthToken = function(){
 	var user = this;
 	var access = 'auth';
 	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
@@ -50,6 +50,26 @@ UserSchema.methods.generateAuthToken = function() {
 		return token;
 	});
 	
+};
+
+UserSchema.statics.findByToken = function(token){
+	var User = this;
+	var decoded;
+	
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch(e) {
+		/* return new Promise((resolve, reject) =>{
+			reject();
+		}); */
+		return Promise.reject();
+	}
+	
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token,
+		'tokens.access': 'auth'
+	})
 };
  
 var User = mongoose.model('User', UserSchema);
