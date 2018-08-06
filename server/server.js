3e37b77b7d1222130561d83f010ffa2d8084d048
gopal <gopal.singh@niit-tech.com>
2018-08-06 05:25:33 -0400

    Add POST /users route and token

--- server/server.js
+++ server/server.js
@@ -94,7 +94,20 @@ app.patch('/todos/:id', (req, res) => {
 		res.status(400).send();
 	})
 	
-})
+});
+
+app.post('/users', (req, res) => {
+	var body = _.pick(req.body, ['email', 'password']);
+	var user = new User(body);
+	
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) =>{
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		res.status(400).send(e);
+	})
+});
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000');
